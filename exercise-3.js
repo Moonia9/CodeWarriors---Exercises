19807a7e3655f620e498ae1bbd17d83a83a38075
Enia Munteanu <75390874+Moonia9@users.noreply.github.com>
2021-03-01 14:19:01 +0100

    updated the solution for exercise-3.js

--- exercise-3.js
+++ exercise-3.js
@@ -1,5 +1,17 @@
 //remove the spaces from the string, then return the resultant string.
+//using TRIM method
 function noSpace(x){
     return x.trim();
 }
 console.log(noSpace("   hello  "));
+
+//using REPLACE method
+function noWhitespaceInString(x){
+    return x.replace(/\s/g, '');
+}
+console.log(noWhitespaceInString("   hello  "));
+
+/* About the .replace()
+/ : it marks the beginning and the end of a pattern
+\s : matches all space characters a.k.a. "match whitespace"
+g : is a flag which means global i.e. match all whitespace,not just the first one*/
