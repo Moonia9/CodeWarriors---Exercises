987fcf243cb6a630137ec47d990ad272ed275a41
Enia Munteanu <75390874+Moonia9@users.noreply.github.com>
2021-03-01 14:27:10 +0100

    updated solution for exercise-4

---
+++ exercise-4.js
@@ -0,0 +1,9 @@
+//Given a number, find its opposite
+function oppositeNumber(num){
+    return(-num);
+}
+console.log(oppositeNumber(10));
+
+//OR
+const opposite = number => -number;
+console.log(opposite(80));
